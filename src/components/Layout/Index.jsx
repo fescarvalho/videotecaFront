e8cf7b3ec1bc9a95e75bf1082189c60bd1869773
Fernando Carvalho <fernando_scarvalho@hotmail.com>
2022-08-01 19:21:56 -0300

    first commit

---
+++ src/components/Layout/Index.jsx
@@ -0,0 +1,10 @@
+import Header from "../Header";
+import { Container } from "./styles";
+
+export default function Layout() {
+  return (
+    <Container>
+      <Header />
+    </Container>
+  );
+}
